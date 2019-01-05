270732b862e1c2b3650ba8482fa73364e89f9ff6
Jan Tatham <jan@sebity.com>
2019-01-05 01:38:13 +0000

    Initial Commit

---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <app-server-status></app-server-status>
+    </div>
+</template>
+
+<script>
+import ServerStatus from './ServerStatus'
+export default {
+  components: {
+    'app-server-status': ServerStatus
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import Home from './components/Home'
+import router from './router'
+
+Vue.component('app-servers', Home)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
